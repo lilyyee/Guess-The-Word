4d407af0c80c081d36704c184be85ed3391c2890
lilyyee <emaillilyyee@gmail.com>
2021-07-28 13:49:43 -0700

    Display word and guessed letters

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ const validateInput = function (input){
 };
 
 
-// Create function to capture input
+// Create function to store input
 const makeGuess = function(guess){
     guess = guess.toUpperCase();
     if (guessedLetters.includes(guess)){
@@ -58,6 +58,44 @@ const makeGuess = function(guess){
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+// Create function to show the guessed letters
+const showGuessedLetters = function (){
+    guessedLettersElement.innerHTML = "";  // Clear list first
+    for (const letter of guessedLetters){
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+
+// Create function to update word in progress
+const updateWordInProgress = function (guessedLetters){
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray){
+        if (guessedLetters.includes(letter)){
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
+};
+
+
+// Create function to check if player won
+const checkIfWin = function(){
+    if (word.toUpperCase()===wordInProgress.innerText){
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word!  Congrats!</p>`;
     }
 };
 
